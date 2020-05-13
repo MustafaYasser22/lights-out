1c451338c0d91582843e828f37f0d2f9ddb11b02
mustafayasser22 <sasayasser22@gmail.com>
2020-05-13 12:43:21 +0200

    Updates

---
+++ precache-manifest.8ed1a5914375eaf58bb935b1388a6679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "e52cab32e7dbaf97e673",
+    "url": "/lights-out/static/js/main.e52cab32.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/lights-out/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "e52cab32e7dbaf97e673",
+    "url": "/lights-out/static/css/main.347fb3da.chunk.css"
+  },
+  {
+    "revision": "c27c92351ba6e039dc2b059a2ed3f402",
+    "url": "/lights-out/index.html"
+  }
+];
